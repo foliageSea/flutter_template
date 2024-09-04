7811cd1cb4cd877fe31fcbc708264a5480e3b6a7
foliagesea <foliagesea@gmail.com>
2024-09-04 01:00:27 +0000

    chore: code

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_template/pages/login/login_controller.dart';
 import 'package:flutter_template/fs_widgets/fs_app_bar.dart';
 import 'package:get/get.dart';
-import 'package:icons_plus/icons_plus.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -68,10 +67,6 @@ class _LoginPageState extends State<LoginPage> {
             },
             child: const Text('登录'),
           ),
-        ),
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(Bootstrap.github),
         ),
       ],
     );
