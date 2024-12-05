20421ae1c7fcba1525409c148b3eeef4e7ca4cf0
foliageSea <1742262457@qq.com>
2024-12-05 16:34:51 +0800

    chore: code

--- lib/interceptors/dio_error_interceptor.dart
+++ lib/interceptors/dio_error_interceptor.dart
@@ -7,15 +7,21 @@ typedef DioErrorInterceptorFunc = Function(
 );
 
 class DioErrorInterceptor extends Interceptor {
-  final Map<DioExceptionType, Function> errorHandlers = {
-    DioExceptionType.connectionTimeout: () => showErrorToast("连接超时，请检查网络连接"),
-    DioExceptionType.sendTimeout: () => showErrorToast("发送请求超时，请稍后重试"),
-    DioExceptionType.receiveTimeout: () => showErrorToast("接收响应超时，请稍后重试"),
-    DioExceptionType.badCertificate: () => showErrorToast("证书验证失败，请检查您的网络设置"),
-    DioExceptionType.badResponse: () => showErrorToast("服务器响应错误，请稍后重试"),
-    DioExceptionType.cancel: () => showErrorToast("请求已被取消"),
-    DioExceptionType.connectionError: () => showErrorToast("连接错误，请检查网络状态"),
-    DioExceptionType.unknown: () => showErrorToast("发生未知错误，请稍后重试"),
+  final Map<DioExceptionType, DioErrorInterceptorFunc> errorHandlers = {
+    DioExceptionType.connectionTimeout: (err, handler) =>
+        showErrorToast("连接超时，请检查网络连接"),
+    DioExceptionType.sendTimeout: (err, handler) =>
+        showErrorToast("发送请求超时，请稍后重试"),
+    DioExceptionType.receiveTimeout: (err, handler) =>
+        showErrorToast("接收响应超时，请稍后重试"),
+    DioExceptionType.badCertificate: (err, handler) =>
+        showErrorToast("证书验证失败，请检查您的网络设置"),
+    DioExceptionType.badResponse: (err, handler) =>
+        showErrorToast("服务器响应错误，请稍后重试"),
+    DioExceptionType.cancel: (err, handler) => showErrorToast("请求已被取消"),
+    DioExceptionType.connectionError: (err, handler) =>
+        showErrorToast("连接错误，请检查网络状态"),
+    DioExceptionType.unknown: (err, handler) => showErrorToast("发生未知错误，请稍后重试"),
   };
 
   @override
