671011a730ddc32faa70beb844e1c99967920edc
foliageSea <1742262457@qq.com>
2024-12-06 17:35:04 +0800

    chore: 优化构建脚本

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # 计时开始
 start=`date +%s`
@@ -9,45 +10,55 @@ cd "$(dirname "$0")"
 # /
 cd ..
 
+
+echo "🚚 开始构建 Android 项目..."
+echo "📌 当前路径: $(pwd)"
+# fvm 路径
+FVM_CMD="/c/tools/fvm/fvm.bat"
+# 读取版本号
+VERSION=$(grep 'version:' pubspec.yaml | cut -d '+' -f 1 | awk '{print $2}')
+echo "📌 项目版本号: $VERSION"
+# 读取项目名称
+PROJECT_NAME=$(grep '^name:' pubspec.yaml | awk '{print $2}')
+echo "📦 项目名称: $PROJECT_NAME"
+# 编译文件名
+BUILD_FILE_NAME="${PROJECT_NAME}_$VERSION"
+# 编译输出目录
+OUTPUT_DIR="build\\app\\outputs\\apk\\release\\"
+
+
+# 国内镜像
 #export PUB_HOSTED_URL="https://pub.flutter-io.cn"
 #export FLUTTER_STORAGE_BASE_URL="https://storage.flutter-io.cn"
 
-
-echo "🚀配置代理"
+# 代理
+echo "⚡️ 配置代理"
 export https_proxy=http://127.0.0.1:7890; export http_proxy=http://127.0.0.1:7890;
 
-echo "🚀清理缓存"
-/c/tools/fvm/fvm.bat flutter clean
+echo "🚧 清理缓存"
+# $FVM_CMD flutter clean > /dev/null 2>&1
 
-echo "🚀安装依赖"
-/c/tools/fvm/fvm.bat flutter pub get
+echo "🍱 安装依赖"
+$FVM_CMD flutter pub get > /dev/null 2>&1
 
-echo "🚀正在编译Flutter项目..."
-/c/tools/fvm/fvm.bat flutter build apk --release -v
+echo "🚀 正在编译Flutter项目"
+$FVM_CMD flutter build apk --release > /dev/null 2>&1
 
 # 检查编译是否成功
 if [ $? -eq 0 ]; then
-    # 读取版本号
-    VERSION=$(grep 'version:' pubspec.yaml | cut -d '+' -f 1 | awk '{print $2}')
-    echo "🚀项目版本号: $VERSION"
-
-
-    # 读取项目名称
-    PROJECT_NAME=$(grep '^name:' pubspec.yaml | awk '{print $2}')
-    echo "🚀项目名称: $PROJECT_NAME"
-
     # 修改APK文件名
-    mv build/app/outputs/apk/release/app-release.apk build/app/outputs/apk/release/${PROJECT_NAME}_$VERSION.apk
+    mv ${OUTPUT_DIR}app-release.apk ${OUTPUT_DIR}${BUILD_FILE_NAME}.apk
  
     # 打开包含APK文件的目录
-    start 'build\\app\\outputs\\apk\\release'
+    start $OUTPUT_DIR
+
+    echo "📦 输出文件名: ${BUILD_FILE_NAME}.apk"
 
     # 计时结束
     end=`date +%s`
     runtime=$((end-start))
-
-    echo "🚀编译完成，耗时 $runtime 秒"
+    echo "🐳 编译完成，耗时 $runtime 秒"
 else
-    echo "❌编译失败"
+    echo "❌ 编译失败"
     exit 1
 fi
--- scripts/build_windows.sh
+++ scripts/build_windows.sh
@@ -1,55 +1,66 @@
 #!/bin/bash
+set -e
 
 # 计时开始
 start=`date +%s`
 
+# /scripts
 cd "$(dirname "$0")"
-
+# /
 cd ..
 
+echo "🚚 开始构建 Windows 项目..."
+echo "📌 当前路径: $(pwd)"
 
-# export PUB_HOSTED_URL="https://pub.flutter-io.cn"
-# export FLUTTER_STORAGE_BASE_URL="https://storage.flutter-io.cn"
+# fvm 路径
+FVM_CMD="/c/tools/fvm/fvm.bat"
+# 读取版本号
+VERSION=$(grep 'version:' pubspec.yaml | cut -d '+' -f 1 | awk '{print $2}')
+echo "📌 项目版本号: $VERSION"
+# 读取项目名称
+PROJECT_NAME=$(grep '^name:' pubspec.yaml | awk '{print $2}')
+echo "📦 项目名称: $PROJECT_NAME"
+# 编译文件名
+BUILD_FILE_NAME="${PROJECT_NAME}_$VERSION"
+# 编译输出目录
+OUTPUT_DIR="build\\windows\\x64\\runner\\"
 
-echo "🚀配置代理"
+
+# 国内镜像
+#export PUB_HOSTED_URL="https://pub.flutter-io.cn"
+#export FLUTTER_STORAGE_BASE_URL="https://storage.flutter-io.cn"
+
+# 代理
+echo "⚡️ 配置代理"
 export https_proxy=http://127.0.0.1:7890; export http_proxy=http://127.0.0.1:7890;
 
-echo "🚀清理缓存"
-/c/tools/fvm/fvm.bat flutter clean
+echo "🚧 清理缓存"
+# $FVM_CMD flutter clean > /dev/null 2>&1
 
-echo "🚀安装依赖"
-/c/tools/fvm/fvm.bat flutter pub get
+echo "🍱 安装依赖"
+$FVM_CMD flutter pub get > /dev/null 2>&1
 
-
-echo "🚀正在编译Flutter项目"
-/c/tools/fvm/fvm.bat flutter build windows -v
+echo "🚀 正在编译Flutter项目"
+$FVM_CMD flutter build windows > /dev/null 2>&1
 
 # 检查编译是否成功
 if [ $? -eq 0 ]; then
-    # 读取版本号
-    VERSION=$(grep 'version:' pubspec.yaml | cut -d '+' -f 1 | awk '{print $2}')
-    echo "🚀项目版本号: $VERSION"
 
-    # 读取项目名称
-    PROJECT_NAME=$(grep '^name:' pubspec.yaml | awk '{print $2}')
-    echo "🚀项目名称: $PROJECT_NAME"
     # 压缩 Release 目录
-    # 
-    # build\\windows\\x64\\runner\\Release
-    ZIP_NAME="build\\windows\\x64\\runner\\${PROJECT_NAME}_v${VERSION}.zip"
-    echo "🚀正在压缩 Release 目录到 $ZIP_NAME..."
-    powershell.exe -command "Compress-Archive -Path build\\windows\\x64\\runner\\Release\\* -DestinationPath $ZIP_NAME -Force"
-    echo "🚀压缩完成"
+    ZIP_NAME="${OUTPUT_DIR}${BUILD_FILE_NAME}.zip"
+    echo "📦 正在压缩 Release 目录到 $ZIP_NAME..."
+    powershell.exe -command "Compress-Archive -Path ${OUTPUT_DIR}\\Release\\* -DestinationPath $ZIP_NAME -Force"
+    echo "📦 压缩完成"
     # 打开目录
-    start 'build\\windows\\x64\\runner'
+    start $OUTPUT_DIR
+    echo "📦 输出文件名: ${ZIP_NAME}"
 
     # 计时结束
     end=`date +%s`
     runtime=$((end-start))
 
-    echo "🚀编译完成，耗时 $runtime 秒"
-
+    echo "🐳 编译完成，耗时 $runtime 秒"
 else
-    echo "❌编译失败"
+    echo "❌ 编译失败"
     exit 1
 fi
