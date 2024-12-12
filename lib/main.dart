e851e600443b82b08d2e4cb3ca4ffcd2fa4ff89d
foliageSea <1742262457@qq.com>
2024-12-12 09:54:03 +0800

    feat: 应用启动异常页面

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,18 @@ import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:flutter_template/global.dart';
 
 import 'app.dart';
+import 'widgets/error_app.dart';
 
 void main() {
-  Global.initApp().then(
-    (_) => runApp(
+  Global.initApp().then((_) {
+    runApp(
       Phoenix(child: const MainApp()),
-    ),
-  );
+    );
+  }, onError: (error) {
+    runApp(
+      ErrorApp(
+        error: error,
+      ),
+    );
+  });
 }
