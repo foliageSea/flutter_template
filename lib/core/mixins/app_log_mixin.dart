14db0876c0e6924e71bea89f6ef273387542e427
foliageSea <1742262457@qq.com>
2025-05-16 17:17:19 +0800

    feat: SignalrClient

--- lib/core/mixins/app_log_mixin.dart
+++ lib/core/mixins/app_log_mixin.dart
@@ -4,4 +4,6 @@ mixin AppLogMixin {
   void Function(dynamic message) log = AppLogger().log;
   void Function(dynamic message) info = AppLogger().info;
   void Function(dynamic message) warning = AppLogger().warning;
+  void Function(Object exception, [StackTrace? stackTrace, dynamic msg])
+      handle = AppLogger().handle;
 }
---
+++ lib/core/utils/signalr_client.dart
@@ -0,0 +1,106 @@
+import 'dart:convert';
+
+import 'package:flutter_template/core/mixins/app_log_mixin.dart';
+import 'package:signalr_netcore/iretry_policy.dart';
+import 'package:signalr_netcore/json_hub_protocol.dart';
+import 'package:signalr_netcore/signalr_client.dart';
+
+abstract class SignalrClientAble {
+  void init(SignalrClientOption option);
+
+  Future<void>? start();
+
+  Future<void> stop();
+
+  HubConnectionState? getHubConnectionState();
+
+  Future<Object?> invoke(String methodName, {List<Object>? args});
+
+  void on(String methodName, MethodInvocationFunc newMethod);
+}
+
+class SignalrClient with AppLogMixin implements SignalrClientAble {
+  static SignalrClient? _signalrClient;
+
+  SignalrClient._();
+
+  factory SignalrClient() {
+    _signalrClient ??= SignalrClient._();
+    return _signalrClient!;
+  }
+
+  late HttpConnectionOptions _httpConnectionOptions;
+  late String _url;
+  late HubConnection _hubConnection;
+
+  @override
+  void init(SignalrClientOption option) {
+    _httpConnectionOptions = HttpConnectionOptions(
+      accessTokenFactory: () async {
+        return option.token;
+      },
+      skipNegotiation: true,
+      transport: HttpTransportType.WebSockets,
+      requestTimeout: 2000,
+    );
+    _url = "${option.url}?${option.key}=${jsonEncode(option.data)}";
+    _hubConnection = HubConnectionBuilder()
+        .withUrl(
+          _url,
+          options: _httpConnectionOptions,
+        )
+        .withHubProtocol(JsonHubProtocol())
+        .withAutomaticReconnect(reconnectPolicy: AlwaysRetryPolicy())
+        .build();
+
+    _hubConnection.onclose(({Exception? error}) async {
+      handle(error as Object, StackTrace.current, 'SignalR连接断开');
+    });
+  }
+
+  @override
+  Future<void>? start() {
+    return _hubConnection.start();
+  }
+
+  @override
+  Future<void> stop() {
+    return _hubConnection.stop();
+  }
+
+  @override
+  HubConnectionState? getHubConnectionState() {
+    return _hubConnection.state;
+  }
+
+  @override
+  Future<Object?> invoke(String methodName, {List<Object>? args}) {
+    return _hubConnection.invoke(methodName, args: args);
+  }
+
+  @override
+  void on(String methodName, MethodInvocationFunc newMethod) {
+    _hubConnection.on(methodName, newMethod);
+  }
+}
+
+class SignalrClientOption {
+  String url;
+  String key;
+  String token;
+  Map<String, dynamic> data = {};
+
+  SignalrClientOption({
+    required this.url,
+    required this.key,
+    required this.token,
+    required this.data,
+  });
+}
+
+class AlwaysRetryPolicy implements IRetryPolicy {
+  @override
+  int? nextRetryDelayInMilliseconds(RetryContext retryContext) {
+    return 10 * 1000;
+  }
+}
